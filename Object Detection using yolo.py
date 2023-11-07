ed6237bbeb4702575c301aa004738d8e5c05f47d
aren-prime <132383378+aren-prime@users.noreply.github.com>
2023-11-07 09:10:25 +0530

    Add files via upload

---
+++ Object Detection using yolo.py
@@ -0,0 +1,74 @@
+import cv2
+import numpy as np
+
+yolo = cv2.dnn.readNet("D:/PYthon/yolo/yolo obj detection (coco names)/yolov3.weights", "D:/PYthon/yolo/yolo obj detection (coco names)/yolov3.cfg")
+classes = []
+
+with open("D:/PYthon/yolo/yolo obj detection (coco names)/coco.names", "r") as file:
+    classes = [line.strip() for line in file.readlines()]
+layer_names = yolo.getLayerNames()
+output_layers = [layer_names[i-1] for i in yolo.getUnconnectedOutLayers()]
+
+colorRed = (255,0,0)
+colorGreen = (0,255,0)
+
+# #Loading Images
+# name = "D:/PYthon/yolo/yolo obj detection (coco names)/images (16) (22).jpeg.jpg"
+# img = cv2.imread(name)
+# height, width, channels = img.shape
+# # Loading Images
+
+# #To use a video file as input 
+cap = cv2.VideoCapture("F:/lil/testing_datas/catndog.mp4")
+# cap = cv2.VideoCapture(0)
+
+while True:
+    _, img = cap.read()
+    height, width, channels = img.shape
+# #To use a video file as input 
+
+    # # Detecting objects
+    blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+
+    yolo.setInput(blob)
+    outputs = yolo.forward(output_layers)
+
+    class_ids = []
+    confidences = []
+    boxes = []
+    for output in outputs:
+        for detection in output:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+    for i in range(len(boxes)):
+        if i in indexes:
+            x, y, w, h = boxes[i]
+            label = str(classes[class_ids[i]])
+            cv2.rectangle(img, (x, y), (x + w, y + h), colorGreen, 3)
+            cv2.putText(img, label, (x, y + 10), cv2.FONT_HERSHEY_PLAIN, 2, colorRed, 4)
+
+    cv2.imshow("Image", img)
+    key = cv2.waitKey(1)
+    if key==27:
+        break
+# #for saving image 
+# cv2.imwrite("D:\PYthon\yolo\res.mp4",img)
+# cv2.waitKey(0)
+#for saving image 
+cap.release()
+cv2.destroyAllWindows()
